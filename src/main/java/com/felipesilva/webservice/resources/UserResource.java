9f6b34304c689141f8cd405547e3fbdaef28cbc5
Felipe Silva <felipesilva32100@gmail.com>
2021-03-17 14:29:52 -0300

    User entity and user resource

---
+++ src/main/java/com/felipesilva/webservice/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.felipesilva.webservice.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.felipesilva.webservice.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "marial@gmail.com", "3333333", "123");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
